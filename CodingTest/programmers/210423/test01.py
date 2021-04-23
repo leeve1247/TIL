6ae3d87af0e3046574ceb1c502126ca5a10e912a
leeve1247 <multirkh@gmail.com>
2021-04-23 23:38:01 +0900

    앞 뒤 끝에것만 체크하면 되는 것을...

--- CodingTest/programmers/210423/test01.py
+++ CodingTest/programmers/210423/test01.py
@@ -4,12 +4,24 @@ def solution(lines):
     for line in lines:
         ttime=line.split(' ')
         hms = ttime[1].split(':')
-        endighms = (60*60*int(hms[0])+60*int(hms[1])+float(hms[2]))*1000
+        endighms = (60*60*int(hms[0])+60*int(hms[1])+float(hms[2]))*1000 
         strdighms = endighms - (float(ttime[2][:-1])*1000) + 1
         diglines.append([int(strdighms),int(endighms)])
     for digline in diglines:
-        p = digline[0]
-        while p <= digline[1]:
+            p = digline[0]
+            tcount = 0
+            for a in diglines:
+                if a[1] < p :
+                    tcount += 0
+                elif p - 999 < a[0]:
+                    tcount += 0
+                else:
+                    tcount += 1
+            if tcount > count :
+                count = tcount        
+            p += 1
+
+            p = digline[1]
             tcount = 0
             for a in diglines:
                 if a[1] < p :
@@ -20,5 +32,5 @@ def solution(lines):
                     tcount += 1
             if tcount > count :
                 count = tcount        
-            p += 1
+            p += 1  
     return count
