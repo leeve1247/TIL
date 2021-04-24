926ef1172fd71da75d166d707ce0894adb9c4fba
leeve1247 <multirkh@gmail.com>
2021-04-24 12:21:36 +0900

    210424(2) 동적프로그래밍(피보나치 수열)

---
+++ CodingTest/programmers/210424/test01.py
@@ -0,0 +1,37 @@
+N = 1
+number = 1
+
+def calculate(s1,s2):
+    result = []
+    for a in s1:
+        for b in s2:
+            result.append(a + b)
+            result.append(a * b)
+            result.append(a - b)
+            if b != 0:result.append(int(a/b))
+    return result
+s = {}
+found = False
+for i in range(1,9):
+    s[i] = set()
+    yunsok = 0
+    for j in range(0, i):
+        yunsok += 10**j*N
+    s[i].add(yunsok)
+    if number in s[i]:
+        resultt = i
+        found = True
+        break
+
+if not found:
+    resultt = -1
+    for j in range(1,9):
+        for i in range(1,j):
+            s[j].update(calculate(s[i],s[j-i]))
+            if number in s[j]:
+                resultt = j
+                found = True
+                break
+        if found :
+            break
+print(resultt)
