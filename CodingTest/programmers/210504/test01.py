43e561e1e8950349ae2584b71995364795b36ab0
leeve1247 <multirkh@gmail.com>
2021-05-05 00:12:15 +0900

    210504 문제풀이 수정

--- CodingTest/programmers/210504/test01.py
+++ CodingTest/programmers/210504/test01.py
@@ -1,45 +1,52 @@
-def solution(s):
-    def dividing(s,num):
-        lis = []
-        tempstr = ""
-        for i in range(len(s)):
-            tempstr += s[i]
-            if i%num == 0:
-                lis.append(tempstr)
-                tempstr = ""
-            else:
-                continue
-        print(lis)
-        return lis
-    
-    def archinize(lis):
-        count = 0
-        temp = "-"
-        result = ""
-        for i in lis:
-            if temp != i:
-                if temp != '-':
-                    if count != 0:
-                        result += str(count+1) + str(temp)
-                    else:
-                        result += str(temp)
-                temp = i
-                count = 0
-            else:
-                count += 1
+#문자열 나누기
+def dividing(s,num):
+    lis = []
+    tempstr = ""
+    for i in range(len(s)):
+        tempstr += s[i]
+        if (i+1)%num == 0:
+            lis.append(tempstr)
+            tempstr = ""
+        else:
+            continue
+    if tempstr != "" :
+        lis.append(tempstr)
+    return lis
+
+#문자열 압축하기
+def archinize(lis):
+    count = 0
+    temp = "-"
+    result = ""
+    for i in lis:
+        if temp != i:
+            if temp != '-':
+                if count != 0:
+                    result += str(count+1) + str(temp)
+                else:
+                    result += str(temp)
+            temp = i
+            count = 0
+        else:
+            count += 1
+    if count != 0:
         result += str(count+1) + str(temp)
-        return result
-      
-    
-    
-    if s == "aabbaccc":
-        #1개로 자르기
-        # lis = list(s)
-        lis = dividing(s,2)
-        result = archinize(lis)
-        print(result)
-        
     else :
-        print("진행안함")
-    answer = 0
+        result += str(temp)
+    return result
+
+def solution(s):
+    if len(s) == 1:
+        answer = 1
+    else:
+        #문자열 자르기
+        gatsu = []
+        n = 1
+        #n개로 자르기, N의 갯수는 (len(s)/2 까지,)
+        while n <= len(s)//2:
+            lis = dividing(s,n)
+            gatsu.append(len(archinize(lis)))
+            n += 1
+        answer = min(gatsu)
+        
     return answer
